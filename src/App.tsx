140b7d517b2dd795ebb2f61a2f2cf298176ceb7b
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2023-12-13 15:21:57 +0100

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import AdminDashboard from './Admin/AdminDashboard.jsx'
+
+import AdminDashboard from "./Admin/AdminDashboard"
+
+
+const App = () => {
+  return (
+    <>
+      <AdminDashboard/>
+    </>
+  )
+}
+
+export default App
